93357449e22545a7fc606e90806ad031885c3d9f
Jakub Tomiczek <tomiczekkuba01@gmail.com>
2023-12-28 21:03:02 +0100

    Initial Commit

---
+++ WordTermin/WordTerminApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordTerminApp.swift
+//  WordTermin
+//
+//  Created by Jakub Tomiczek on 28/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordTerminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
